bd91390abde890d2c97cb8be8b531832c20fa279
Marcel Schilling <marcel.schilling@mdc-berlin.de>
2017-03-26 09:08:27 +0200

    moved message definitions into separate module

---
+++ messages.py
@@ -0,0 +1,76 @@
+# cdshelf message definitions
+# Copyright (C) 2017  Marcel Schilling
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#######################
+# general information #
+#######################
+
+# file:     messages.py
+# created:  2017-03-26
+# author:   Marcel Schilling <marcel.schilling@mdc-berlin.de>
+# license:  GNU Affero General Public License Version 3 (GNU AGPL v3)
+# purpose:  define messages for cdshelf Audio CD backup & conversion tool
+
+
+######################################
+# change log (reverse chronological) #
+######################################
+
+# 2017-03-26: initial version (help, usage & license)
+
+
+#######################
+# message definitions #
+#######################
+
+# define usage message
+usage="""\
+usage: cdshelf <command> [<command> ...]
+
+The following commands are currently supported:
+
+help	print help message
+usage	show usage
+license	print license
+"""
+
+# define help message
+help="""\
+cdshelf
+
+Audio CD backup & conversion tool
+
+
+""" + usage
+
+# define license message
+license="""\
+Copyright (C) 2017  Marcel Schilling
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
